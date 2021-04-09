677a35c772fc2087f72c7f254f48d1d0d91bc393
alexbanks2399 <38721728+alexbanks2399@users.noreply.github.com>
2021-04-09 14:33:06 +0100

    Made generator into a function. Output file name is now dynamic for device name. Device name, date, and time are now in comment line of config. Variable names now make more sense.

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import csv
+import datetime
 from jinja2 import Template
 
 source_file = "switch_data.csv"
 interface_template_file = "switchport-interface-template.j2"
 
-interface_configs = ""
 
-with open(interface_template_file) as f:
-    interface_template = Template(f.read(), keep_trailing_newline=True)
+def config_generator():
+    generated_configs = ""
 
-with open(source_file) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        interface_config = interface_template.render(
-            device_name=row["Device Name"],
-            interface=row["Interface"],
-            connected=row["Connected"],
-            link=row["Link"],
-            purpose=row["Purpose"],
-            vlan=row["VLAN"]
-        )
-        interface_configs += interface_config
+    with open(interface_template_file) as f:
+        interface_template = Template(f.read(), keep_trailing_newline=True)
+    with open(source_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            generated_config = interface_template.render(
+                device_name=row["Device Name"],
+                date_time=datetime.datetime.now().isoformat(timespec='minutes'),
+                interface=row["Interface"],
+                connected=row["Connected"],
+                link=row["Link"],
+                purpose=row["Purpose"],
+                vlan=row["VLAN"]
+            )
+            device_name = row["Device Name"]
+            generated_configs += generated_config
 
-with open("interface_configs.txt", "w") as f:
-    f.write(interface_configs)
+    with open(device_name + "_config.txt", "a") as f:
+        f.write(generated_configs)
+
+
+config_generator()
