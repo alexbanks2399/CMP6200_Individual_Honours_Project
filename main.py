227247b1d2bb7c44155e75b444cd808bd729c8f9
alexbanks2399 <38721728+alexbanks2399@users.noreply.github.com>
2021-04-19 14:30:18 +0100

    Removed dynamic file input. Couldn't get working. Need to move onto testing rather than implementing a "nice to have" feature.

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
 import csv
+import datetime
+import getpass
+from netmiko import Netmiko
 from jinja2 import Template
+
+# Pseudo code
+# Ask user for input files; device data, template file
+# Generate config to apply
+# Ask user for device credentials
+# Apply generated config to given device
+
+
+start_time = datetime.datetime.now()
 
 source_file = "switch_data.csv"
 interface_template_file = "switchport-interface-template.j2"
 
-interface_configs = ""
 
-with open(interface_template_file) as f:
-    interface_template = Template(f.read(), keep_trailing_newline=True)
+def config_generator():
+    generated_configs = ""
+    device_ips = ""
 
-with open(source_file) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        interface_config = interface_template.render(
-            interface=row["Interface"],
-            connected=row["Connected"],
-            link=row["Link"],
-            purpose=row["Purpose"],
-            vlan=row["VLAN"]
-        )
-        interface_configs += interface_config
+    with open(interface_template_file) as f:
+        interface_template = Template(f.read(), keep_trailing_newline=True, autoescape=True)
+    with open(source_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            generated_config = interface_template.render(
+                device_name=row["Device Name"],
+                date_time=datetime.datetime.now().isoformat(timespec='minutes'),
+                interface=row["Interface"],
+                connected=row["Connected"],
+                link=row["Link"],
+                purpose=row["Purpose"],
+                vlan=row["VLAN"]
+            )
+            device_name = row["Device Name"]
+            device_ip = row["Device IP"]
 
-with open("interface_configs.txt", "w") as f:
-    f.write(interface_configs)
+            generated_configs += generated_config
+            device_ips += device_ip + "\n"
+
+    with open(device_name + "_config.txt", "a") as f:
+        f.write(generated_configs)
+    with open("device_list.txt", "a") as f:
+        f.write(device_ips + "\n")
+
+
+def config_applier():
+    sites = open("device_list.txt")
+    devicelist = []
+    username = input("Input device username: ")
+    password = getpass.getpass("Input device password: ")
+    secret = getpass.getpass("Input device secret: ")
+
+    for line in sites:
+        line = line.rstrip()
+        devicelist.append(line)
+
+    for host in devicelist:
+        net_connect = Netmiko(
+            host,
+            username=username,
+            password=password,
+            device_type="cisco_ios",
+            secret=secret)
+
+
+config_generator()
+# config_applier()
